24a72d34f1e5d8a920fa88d53ab90f15e74769a1
Ibrahim Hicham <himasalama102@gmail.com>
2026-06-16 22:39:35 +0300

    Reorganize ASGI application setup and imports

--- HomemadeFood/asgi.py
+++ HomemadeFood/asgi.py
@@ -10,14 +10,19 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from orders.middleware import TokenAuthMiddleware
 from channels.security.websocket import AllowedHostsOriginValidator
-from orders.routing import websocket_urlpatterns
 
+# 1. First, set the settings environment variable
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HomemadeFood.settings')
 
+# 2. Second, initialize Django and load all the application registries
 django_asgi_app = get_asgi_application()
 
+# 3. Third, NOW it is completely safe to import your custom middleware and app routes
+from orders.middleware import TokenAuthMiddleware
+from orders.routing import websocket_urlpatterns
+
+# 4. Finally, declare the Protocol Type Router
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
     'websocket': AllowedHostsOriginValidator(
